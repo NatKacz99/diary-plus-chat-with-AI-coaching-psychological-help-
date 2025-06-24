af83fb34f8204f8dfffbedbbcf40d567add6ed0f
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-06-24 21:33:15 +0200

    add navbar styling update

--- frontend/diary-frontend/src/components/Header/Header.jsx
+++ frontend/diary-frontend/src/components/Header/Header.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
 import { Link } from 'react-router-dom';
+import "./Header.css"
 
 function Header() {
   return (
     <header>
       <nav>
-        <Link to="/"><h1>
-        <HighlightIcon />
-        Diary
-      </h1></Link>
+        <div className="navbar">
+          <div>
+            <Link to="/" style={{ textDecoration: "none" }}><h1>
+              <HighlightIcon />
+              Diary
+            </h1></Link>
+          </div>
+          <div className="login-and-registration">
+            <div>
+              <Link to="/signup"><p>Sign Up</p></Link>
+            </div>
+            <div>
+              <Link to="/signin"><p>Sign In</p></Link>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
--- frontend/diary-frontend/src/main.jsx
+++ frontend/diary-frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import App from "./components/App/App";
 import "./assets/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
