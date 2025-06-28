3ae145b02c7e3e567eceedcdaf4821fcfd7e678d
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-06-28 14:02:16 +0200

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,6 +26,10 @@ export async function signup(req, res) {
       return res.status(400).json({ success: false, message: 'Passwords are not the same.' });
     }
 
+    if(password.length < 8){
+      return res.status(400).json({ success: false, message: `The password must consist of at least 8 characters`});
+    }
+
     const checkResultUsername = await db.query("SELECT * FROM users WHERE username = $1", [
       username
     ]);
